25bc55271ce6896276c7ccd35ef9d7b80144c778
IbraimF <ibraim.figueroa@upr.edu>
2017-03-29 17:29:08 -0400

    update busdriver list

--- client/app/components/busdriver/app.busdriverComponent.ts
+++ client/app/components/busdriver/app.busdriverComponent.ts
@@ -5,5 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app/components/busdriver/busdriver.html',
 })
 export class busdriverComponent  {
-
+  busProperties=[
+    {name: "Bus A", driver: "Juan", route: "Route A", status: "active"},
+    {name: "Bus B", driver: "Pedro", route: "Route B", status: "offline"},
+    {name: "Bus C", driver: "Jorge", route: "Route C", status: "offline"}
+    ]
+    
 }
+interface busProperties {
+ name: string;
+ driver: string;
+ route: string;
+status: string;
+}
+
+
