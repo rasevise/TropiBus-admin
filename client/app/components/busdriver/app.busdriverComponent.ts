c0c222db945c082c71afb0448071154a611cf244
rasevise <sebavissepo@gmail.com>
2017-04-04 13:49:38 -0400

    update

--- client/app/components/busdriver/app.busdriverComponent.ts
+++ client/app/components/busdriver/app.busdriverComponent.ts
@@ -20,6 +20,11 @@ export class busdriverComponent{
   driver: Driver = new Driver();
   buses: any[] = [];
   drivers: any[] = [];
+  for(i:number = 0 ; i < this.buses.length;i++){
+
+  
+  console.log(buses[i]);
+  }
   
   // bus = {id: 1, name: 'name', driver: 'driver', route: 'route', status:'status'};
 
@@ -115,6 +120,12 @@ export class busdriverComponent{
     temp_bus.driver = bus.driver;
     temp_bus.route = bus.route;
     temp_bus.status = bus.status;
+
+     for(i:number = 0 ; i < this.buses.length;i++){
+
+  
+  console.log(buses[i]);
+  }
     
     this.bus.name="";
     this.bus.driver="";
