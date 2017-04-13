249480f2dcd09f34ed7c6abcfab7ea1470f428cb
IbraimF <ibraim.figueroa@upr.edu>
2017-04-12 23:15:00 -0400

    Commit

--- routes/messages.js
+++ routes/messages.js
@@ -12,10 +12,7 @@ router.get('/', function(req, res, next){
   db.query('SELECT * FROM Message', sendData);
 
   function sendData(err, results) {
-<<<<<<< HEAD
     console.log("getting messages from server: ");
-=======
->>>>>>> 64ae4d576ac81a1f45d109839454a21c2bee8efe
     Message = results.rows;
     res.json(Message);
   }
@@ -24,7 +21,6 @@ router.get('/', function(req, res, next){
 
 
 router.post('/addMessage', function (req, res, next) {
-<<<<<<< HEAD
    var m_title = req.body.title;
   var m_mess = req.body.messageContent;
   var m_date = req.body.date
@@ -34,7 +30,6 @@ router.post('/addMessage', function (req, res, next) {
   res.contentType('application/json');
   db.query('INSERT INTO Message(admin_id, message_text, message_date, message_title) VALUES($1,$2,$3,$4)', [i],[m_mess],[m_date],[m_title]);
 console.log("helo")
-=======
   var m_title = req.body.title;
   var m_mess = req.body.messageContent;
   // db.query('INSERT INTO Message(admin_id, message_text, message_date, message_title) VALUES(${i},${m_mess},${m_date},${m_title})', sendData);
@@ -52,7 +47,6 @@ console.log("helo")
       res.send(200)
     })
 
->>>>>>> 64ae4d576ac81a1f45d109839454a21c2bee8efe
 });
 
 router.put('/updateMessage', function (req, res, next) {
