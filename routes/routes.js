7cb49697ff4fca3f887e3868e246b02511938f1b
Sebastian Vissepo <Sebastian@Sebastians-MacBook-Air.local>
2017-04-18 16:47:26 -0400

    completed route/stop/map

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const db = require('../server.js');
 
 var getAllRoutes = 'SELECT * FROM Route NATURAL JOIN routepath' //ok 
 var getRoute = 'SELECT * FROM route NATURAL JOIN routepath WHERE route_id = $1'
+var updateRoute = 'UPDATE Route SET route_name=$1, route_description=$2, route_area=$3 WHERE route_id=$4'//ok
 
 routes = [];
 
@@ -25,6 +26,25 @@ router.get('/getRoute', function(req, res, next){
             { console.error(err); res.send("Error " + err); }
         else{
         res.json(result.rows);
+        }
+    });
+});
+
+router.put('/updateRoute', function (req, res, next) {
+    db.query(updateRoute,[req.body.route_name, req.body.route_description,req.body.route_area, req.body.route_id] ,function(err, result) {
+        if (err){ 
+            console.error(err); response.send("Error " + err); 
+        }
+        else {
+            db.query(getAllRoutes, function(err, result){
+
+                if(err){ 
+                    console.error(err); response.send("Error " + err); 
+                }
+                else{
+                res.json(result.rows); 
+                }
+            });
         }
     });
 });
