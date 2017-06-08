c2bfb1df134c45a63ceae64660b6a92f80fbedf0
rasevise <sebavissepo@gmail.com>
2017-06-08 11:55:00 -0400

    fix route update

--- src/server/services/routes.ts
+++ src/server/services/routes.ts
@@ -3,7 +3,7 @@ import * as db from '../db/pg';
 
 var getAllRoutes = 'SELECT * FROM Route NATURAL JOIN routepath';
 var getRoute = 'SELECT * FROM route NATURAL JOIN routepath WHERE route_id = $1';
-var updateRoute = 'UPDATE Route SET route_name=$1, route_description=$2, route_area=$3 WHERE route_id=$4';
+var updateRoute = 'UPDATE Route SET route_name=$1, route_description=$2 WHERE route_id=$3';
 var getBusLocation = 'SELECT gps_latitude, gps_longitude, bus_name FROM bus NATURAL JOIN gps WHERE bus_status=\'Active\'';
 
 export function routes(app: express.Application) {
@@ -33,7 +33,7 @@ app.get(_routesURL + '/getRoute', (req, res, next) => {
 });
 
 app.put(_routesURL + '/updateRoute', (req, res, next) => {
-  db.query(updateRoute,[req.body.route_name, req.body.route_description,req.body.route_area, req.body.route_id], (err:any, result:any) => {
+  db.query(updateRoute,[req.body.route_name, req.body.route_description, req.body.route_id], (err:any, result:any) => {
     if (err) {
       console.error(err); res.send('Error ' + err);
     }else {
